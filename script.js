7532f17479fcb202bec4e1ffa97acd0e75eb1e99
uilton-siqueira <uilton.siqueira@nemu.com.br>
2024-07-31 15:22:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("Uilton é o melhor programador do mundo!");
 console.log(LS.order);
 
-setTimeout(() => {
+setInterval(() => {
   console.log("Passei no timeout!");
   console.log(LS.order);
+  console.log(document.cookie);
 }, 500);
