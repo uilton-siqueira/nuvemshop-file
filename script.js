d0f529b32074efe3ae0130398be74bfa5c5a1eb8
uilton-siqueira <uilton.siqueira@nemu.com.br>
2024-08-02 15:33:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-console.log(LS.order);
+function getCookie(name) {
+  return document.cookie.split(";").find(function(cookie) {
+    return cookie.trim().split("=")[0] == name; 
+  });
+}
 
-setInterval(() => {
-  console.log("Passei no timeout!");
-  console.log(LS.order);
-  console.log(document.cookie);
-}, 1000);
+var timer = setTimeout(function() {
+  var cart = LS.cart;
+
+  if (cart) {
+    var isSent = getCookie(`${cart.id}-nemu-sent`);
+
+    if (isSent) {
+      clearInterval(timer);
+      return;
+    }
+
+    var utmsTrack = getCookie("utmsTrack");
+    var pixelPattern = /nemu_.{10}/;
+
+    try {
+      utmsTrack = decodeURIComponent(utmsTrack);
+    } catch (error) {}
+    
+    var trackingSessionId = utmsTrack.match(pixelPattern)[0];
+
+    if (!trackingSessionId) {
+      clearInterval(timer);
+      return;
+    }
+
+    var body = {
+      trackingSessionId: trackingSessionId,
+      orderId: cart.id
+    }
+
+    fetch("https://eoihhyz2yvxf4vw.m.pipedream.net", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then(function(response) {
+        if (response.status === 200) {
+          const expiresDate = new Date();
+          expiresDate.setHours(expiresDate.getHours() + 1);
+          document.cookie = `${cart.id}-nemu-sent=true; expires=${expiresDate.toUTCString()}; path=/`;
+        }
+      }).finally(function() {
+        clearInterval(timer);
+      });
+
+  }
+}, 500);
